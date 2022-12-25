8e3a56fa0ef471bce3246d98bbe49e01fd6102c3
Jahid Uddin <jahiduddin922@gmail.com>
2022-12-25 22:09:49 +0600

    Add Dll Class

---
+++ HR-Management-System/DLL/EntityModels/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.EntityModel
+{
+    public class Employee
+    {
+        public string 
+    }
+}
+Name , Department , Designation, Email, Mobile No 
