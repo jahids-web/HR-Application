16b7a797a7e57c9181c6d6f65560c1d7c6a2af98
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-02 23:32:02 +0600

    Name Change

--- HR-Management-System/HR_Api/Controllers/EmployeeController.cs
+++ HR-Management-System/HR_Api/Controllers/EmployeeController.cs
@@ -10,77 +10,10 @@ namespace HR_Api.Controllers
     [Route("api/v1{version:apiVersion}/[controller]")]
     public class EmployeeController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            return Ok(EmployeeStatic.GetAllEmployee());
-        }
-
-        [HttpGet("{role}")]
-        public IActionResult GetA(string role)
-        {
-            return Ok(EmployeeStatic.GetAEmployee);
-        }
-
-        [HttpPost]
-        public IActionResult Insert(Employee employee)
-        {
-            return Ok(EmployeeStatic.InsertEmployee(employee));
-        }
-
-        [HttpPut("{role}")]
-        public IActionResult Update(string role, Employee employee)
-        {
-            return Ok(EmployeeStatic.UpdateEmployee (role ,employee));
-        }
-
-        [HttpDelete("{role}")]
-        public IActionResult Delete(string role)
-        {
-            return Ok(EmployeeStatic.DeleteDepartment(role));
-        }
+     
     }
 
-    public static class EmployeeStatic
-    {
-        private static List<Employee> AllEmployee { get; set; } = new List<Employee>();
-
-        public static Employee InsertEmployee(Employee employee)
-        {
-            AllEmployee.Add(employee);
-            return employee;
-        }
-
-        public static List<Employee> GetAllEmployee()
-        {
-            return AllEmployee;
-        }
-
-        public static Employee GetAEmployee(string role)
-        {
-            return AllEmployee.FirstOrDefault(x => x.Role == role);
-        }
-
-        public static Employee UpdateEmployee(string role, Employee employee)
-        {
-            foreach (var aEmployee in AllEmployee)
-            {
-                if (role == aEmployee.Role)
-                {
-                    aEmployee.Name = employee.Name;
-                }
-
-            }
-            return employee;
-        }
-
-        public static Employee DeleteDepartment(string role)
-        {
-            var employee = AllEmployee.FirstOrDefault(x => x.Role == role);
-            AllEmployee = AllEmployee.Where(x => x.Role != employee.Role).ToList();
-            return employee;
-        }
-    }
+    
 
 }
 
---
+++ HR-Management-System/HR_Api/Controllers/HrAdminController.cs
@@ -0,0 +1,83 @@
+﻿using DLL.EntityModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v1{version:apiVersion}/[controller]")]
+    public class HrAdminController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(EmployeeStatic.GetAllEmployee());
+        }
+
+        [HttpGet("{employeId}")]
+        public IActionResult GetA(string employeId)
+        {
+            return Ok(EmployeeStatic.GetAEmployee);
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Employee employee)
+        {
+            return Ok(EmployeeStatic.InsertEmployee(employee));
+        }
+
+        [HttpPut("{employeId}")]
+        public IActionResult Update(string employeId, Employee employee)
+        {
+            return Ok(EmployeeStatic.UpdateEmployee(employeId, employee));
+        }
+
+        [HttpDelete("{employeId}")]
+        public IActionResult Delete(string employeId)
+        {
+            return Ok(EmployeeStatic.DeleteDepartment(employeId));
+        }
+    }
+
+    public static class EmployeeStatic
+    {
+        private static List<Employee> AllEmployee { get; set; } = new List<Employee>();
+
+        public static Employee InsertEmployee(Employee employee)
+        {
+            AllEmployee.Add(employee);
+            return employee;
+        }
+
+        public static List<Employee> GetAllEmployee()
+        {
+            return AllEmployee;
+        }
+
+        public static Employee GetAEmployee(int employeeId)
+        {
+            return AllEmployee.FirstOrDefault(x => x.EmployeeId == employeeId);
+        }
+
+        public static Employee UpdateEmployee(int employeeId, Employee employee)
+        {
+            foreach (var aEmployee in AllEmployee)
+            {
+                if (employeeId == aEmployee.EmployeeId)
+                {
+                    aEmployee.Name = employee.Name;
+                }
+
+            }
+            return employee;
+        }
+
+        public static Employee DeleteDepartment(int employeeId)
+        {
+            var employee = AllEmployee.FirstOrDefault(x => x.EmployeeId == employeeId);
+            AllEmployee = AllEmployee.Where(x => x.EmployeeId != employee.EmployeeId).ToList();
+            return employee;
+        }
+    }
+}
