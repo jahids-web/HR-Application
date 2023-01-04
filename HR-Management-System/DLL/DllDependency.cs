0b0339f1cbb9fbeffc391a3e70ef5b006f80ae77
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-04 23:59:11 +0600

    Add Connection Strings

---
+++ HR-Management-System/DLL/DllDependency.cs
@@ -0,0 +1,21 @@
+﻿using DLL.DataContext;
+using FluentAssertions.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DLL
+{
+    public static class DllDependency
+    {
+        public static void AllDependency(IServiceCollection services, IConfiguration configuration)
+        {
+            //Connection Strings
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HrConnection"));
+            });
+            //  
+        }
+
+    }
+}
