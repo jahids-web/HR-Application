014db3bdf06406bd30c3d54444c41bf5b7d2a4e3
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-04 23:56:26 +0600

    Add New Controller

---
+++ HR-Management-System/HR_Api/Controllers/MainApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v1{version:apiVersion}/[controller]")]
+    public class MainApiController : ControllerBase
+    {
+    }
+}
