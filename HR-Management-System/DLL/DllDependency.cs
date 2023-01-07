ebd77be2481cc5d83d17180341465bd2b2e58afe
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-07 16:16:35 +0600

    Add Migration And Update DataBase

--- HR-Management-System/DLL/DllDependency.cs
+++ HR-Management-System/DLL/DllDependency.cs
@@ -1,23 +1,15 @@
-﻿using DLL.DataContext;
-using FluentAssertions.Common;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DLL
 {
     public static class DllDependency
     {
-        public static void AllDependency(IServiceCollection services, IConfiguration configuration)
-        {
-            //Connection Strings
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(configuration.GetConnectionString("HrConnection"));
-            });
-            //  
-        }
+        //public static void AllDependency(IServiceCollection services, IConfiguration configuration)
+        //{
 
-      
+        //}
+
+
     }
 }
---
+++ HR-Management-System/DLL/Migrations/20230107094802_HrMigration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DLL.Migrations
+{
+    public partial class HrMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(20)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(40)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    Designation = table.Column<string>(type: "nvarchar(30)", nullable: true),
+                    MobileNo = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    Present = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaveReport = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
