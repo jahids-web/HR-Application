88fc5cbe1f7d4bea134e93b8df6ab392bfbf457e
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-08 00:02:05 +0600

    Implement Repositories

--- HR-Management-System/DLL/DllDependency.cs
+++ HR-Management-System/DLL/DllDependency.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DLL.Repositories;
+using FluentAssertions.Common;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DLL
 {
     public static class DllDependency
     {
-        //public static void AllDependency(IServiceCollection services, IConfiguration configuration)
-        //{
-
-        //}
-
+        public static void AllDependency(this IServiceCollection services)
+        {
+            services.AddTransient<IHrAdminRepository, HrAdminRepository>();
+        }
+       
 
     }
 }
