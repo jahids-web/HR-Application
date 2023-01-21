76cbeb48443c32883f236790f7fb9d37f8185e66
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-21 23:59:08 +0600

    Create New DB Context

---
+++ HR-Application/DLL/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DLL.EntityModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DLL.DataContext
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
