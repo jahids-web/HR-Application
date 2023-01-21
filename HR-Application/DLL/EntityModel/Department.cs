87a7703a52281f375cbcdf93fe9655a11de6d6db
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-22 00:02:05 +0600

    Add Some EntityModel

--- HR-Application/DLL/EntityModel/Department.cs
+++ HR-Application/DLL/EntityModel/Department.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,10 @@ namespace DLL.EntityModel
 {
     public class Department
     {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string DepartmentName { get; set; }
     }
 }
--- HR-Application/DLL/EntityModel/Employee.cs
+++ HR-Application/DLL/EntityModel/Employee.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DLL.EntityModel
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Role { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Designation { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        public string MobileNo { get; set; }
+
+        public DateTime Present = DateTime.Now;
+        public DateTime CheckOut = DateTime.Now;
+
     }
 }
