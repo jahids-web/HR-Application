f2d21a1bee11072ead6f10c69e3ee81d5b092f41
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-22 00:02:28 +0600

    Add Connection String

---
+++ HR-Application/DLL/DllDependency.cs
@@ -0,0 +1,26 @@
+﻿using DLL.DataContext;
+using FluentAssertions.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DLL
+{
+    public class DllDependency
+    {
+        public static void AllDependency(IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("HrDatabase")));
+
+
+            //Repository Depandency
+            //services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            //services.AddTransient<IStudentRepository, StudentRepository>();
+            //services.AddTransient<ICourseRepository, CourseRepository>();
+
+            //services.AddTransient<IUnitOfWork.IUnitOfWork, UnitOfWork>();
+
+
+        }
+    }
+}
