483816340d3ebb4a1ea8f257014215e140b7ad7d
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-22 21:40:38 +0600

    Add Migrations

---
+++ HR-Application/DLL/Migrations/20230122153627_ApiMigration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class ApiMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Designation = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(11)", nullable: true),
+                    MobileNo = table.Column<string>(type: "nvarchar(11)", nullable: false),
+                    WorkHour = table.Column<string>(type: "nvarchar(11)", nullable: false),
+                    EarlyLeft = table.Column<string>(type: "nvarchar(11)", nullable: false),
+                    OverTime = table.Column<string>(type: "nvarchar(11)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
