432a7d346e7571131b793825fccfe07de7c3818d
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-23 23:56:47 +0600

    Add Some Methord

--- HR-Application/HR_Api/Controllers/EmployeeController.cs
+++ HR-Application/HR_Api/Controllers/EmployeeController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DLL.EntityModel;
+using DLL.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace HR_Api.Controllers
 {
     public class EmployeeController : MainController
     {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert (Employee employee)
+        {
+            return Ok(await _employeeRepository.Insert(employee));
+        }
     }
 }
