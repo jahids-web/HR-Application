6300110e24cbf69dd14bc861aab9b087af6d85fd
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-24 01:16:21 +0600

    Add All Crud Method

--- HR-Application/HR_Api/Controllers/EmployeeController.cs
+++ HR-Application/HR_Api/Controllers/EmployeeController.cs
@@ -18,7 +18,31 @@ namespace HR_Api.Controllers
         [HttpPost]
         public async Task<IActionResult> Insert (Employee employee)
         {
-            return Ok(await _employeeRepository.Insert(employee));
+            return Ok(await _employeeRepository.InsertAsync(employee));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GatAll()
+        {
+            return Ok(await _employeeRepository.GetAllAsync());
+        }
+
+        [HttpGet("{employeeId}")]
+        public async Task<IActionResult> GatA(int employeeId)
+        {
+            return Ok(await _employeeRepository.GetAAsync(employeeId));
+        }
+
+        [HttpPut("{employeeId}")]
+        public async Task<IActionResult> Update(int employeeId, Employee employee)
+        {
+            return Ok(await _employeeRepository.UpdateAsync(employeeId, employee));
+        }
+
+        [HttpDelete("{employeeId}")]
+        public async Task<IActionResult> Delete(int employeeId)
+        {
+            return Ok (await _employeeRepository.DeleteAsync(employeeId));
         }
     }
 }
