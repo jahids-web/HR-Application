247fb692127d40befd32ce44548a6e30e07a35fd
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-28 11:30:25 +0600

    Implement RepositoryBase

--- HR-Application/DLL/Repositories/IEmployeeRepository.cs
+++ HR-Application/DLL/Repositories/IEmployeeRepository.cs
@@ -9,61 +9,15 @@ using System.Threading.Tasks;
 
 namespace DLL.Repositories
 {
-    public interface IEmployeeRepository
+    public interface IEmployeeRepository : IRepositoryBase<Employee>
     {
-        Task<Employee> InsertAsync(Employee employee); 
-        Task<List<Employee>> GetAllAsync();
-        Task<Employee> GetAAsync(int employeeId);
-        Task<Employee> UpdateAsync(int employeeId, Employee employee);
-        Task<Employee> DeleteAsync(int employeeId);
+
     }
 
-    public class EmployeeRepository : IEmployeeRepository
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     {
-        private readonly ApplicationDbContext _context;
-
-        public EmployeeRepository (ApplicationDbContext context)
+        public EmployeeRepository(ApplicationDbContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public async Task<Employee> InsertAsync(Employee employee)
-        {
-           await _context.Employees.AddAsync(employee);
-            await _context.SaveChangesAsync();
-            return employee;
-        }
-
-        public async Task<List<Employee>> GetAllAsync()
-        {
-            return await _context.Employees.ToListAsync();
-        }
-
-        public async Task<Employee> GetAAsync(int employeeId)
-        {
-            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.EmployeeId == employeeId);
-            return employee;
-        }
-
-        public async Task<Employee> UpdateAsync(int employeeId, Employee employee)
-        {
-            var findEmployee = await _context.Employees.FirstOrDefaultAsync(x => x.EmployeeId == employeeId);
-            findEmployee.EmployeeId = employee.EmployeeId;
-            findEmployee.Name = employee.Name;
-            findEmployee.Email = employee.Email;
-            findEmployee.Status = employee.Status;
-            findEmployee.WorkHour = employee.WorkHour;
-            _context.Employees.Update(findEmployee);
-            await _context.SaveChangesAsync();  
-            return employee;
-        }
-
-        public async Task<Employee> DeleteAsync(int employeeId)
-        {
-            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.EmployeeId == employeeId);
-            _context.Employees.Remove(employee);
-            await _context.SaveChangesAsync();
-            return employee;
         }
     }
 }
