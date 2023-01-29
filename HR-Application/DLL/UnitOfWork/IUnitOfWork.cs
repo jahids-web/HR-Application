6d8736daa08f7cdde25a890d524de2e47ff171e5
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-30 00:46:04 +0600

    Implement UniteOfWork

---
+++ HR-Application/DLL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DLL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IEmployeeRepository EmployeeRepository { get; }
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ HR-Application/DLL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using DLL.DataContext;
+using DLL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+
+        private readonly ApplicationDbContext _context;
+        private bool disposed = false;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private IEmployeeRepository _employeeRepository;
+        
+        public IEmployeeRepository EmployeeRepository =>
+            _employeeRepository ?? new EmployeeRepository(_context);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+     
+    }
+}
