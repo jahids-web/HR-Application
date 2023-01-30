ac54f151c31f9e795612f34daaeb7729c138a919
Jahid Uddin <jahiduddin922@gmail.com>
2023-01-30 23:04:45 +0600

    Implement Some Method

--- HR-Application/HR_Api/Controllers/SuperAdminController.cs
+++ HR-Application/HR_Api/Controllers/SuperAdminController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BLL.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace HR_Api.Controllers
 {
     public class SuperAdminController : MainController
     {
+        private readonly IEmployeeService _employeeService;
+
+        public SuperAdminController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GatAll()
+        {
+            return Ok(await _employeeService.GetAllAsync());
+        }
+
+        [HttpGet("{employeeId}")]
+        public async Task<IActionResult> GatA(int employeeId)
+        {
+            return Ok(await _employeeService.GetAAsync(employeeId));
+        }
     }
 }
