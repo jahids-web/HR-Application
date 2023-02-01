27525b0546ce572be03ccbdf6747c137af6decb9
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-01 23:51:33 +0600

    Add New ModelAdd Some Model

---
+++ HR-Application/DLL/EntityModel/Employee-Wise-Present-Absent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.EntityModel
+{
+    public class Employee_Wise_Present_Absent
+    {
+    }
+}
