0855ff7cdb020db7ecc41229781212222dddf75e
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-01 23:51:33 +0600

    Add Some Model

--- HR-Application/DLL/EntityModel/Employee.cs
+++ HR-Application/DLL/EntityModel/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -24,28 +25,23 @@ namespace DLL.EntityModel
         [Column(TypeName = "nvarchar(5)")]
         public string Status { get; set; }
 
-        [Column(TypeName = "nvarchar(11)")]
-
+        [Column(TypeName = "nvarchar(20)")]
         public int Total_Yearly_Allocated_leave { get; set; }
 
         [Column(TypeName = "nvarchar(12)")]
-
         public string MobileNo { get; set; }
 
-        [Column(TypeName = "nvarchar(5)")]
-
+        [Column(TypeName = "nvarchar(15)")]
         public string Leave { get; set; }
 
         [Column(TypeName = "nvarchar(10)")]
-
         public string IsEmployed { get; set; }
 
-        [Column(TypeName = "nvarchar(5)")]
+        [Column(TypeName = "nvarchar(20)")]
+        public DateTime JoiningDate { get; set; }
 
-        public string JoiningDate { get; set; }
-
-        [Column(TypeName = "nvarchar(5)")]
-        public string MobileNo { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        public DateTime DeparturedDate { get; set; }
 
         [Column(TypeName = "nvarchar(5)")]
         public string WorkHour { get; set; }
