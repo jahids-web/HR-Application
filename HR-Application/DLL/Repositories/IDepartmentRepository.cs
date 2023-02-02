c218d6f4ad2520942830139e5d4c1af76545ee29
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-02 23:26:49 +0600

    Add New Repositories IDepartmentRepository

---
+++ HR-Application/DLL/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using DLL.DataContext;
+using DLL.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories
+{
+    public interface IDepartmentRepository : IRepositoryBase<Department>
+    {
+    }
+
+    public class DepartmentRepository : RepositoryBase<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
