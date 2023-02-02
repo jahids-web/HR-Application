9c03352d5dd68c99c967bc960c622f93022ea3b9
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-02 23:24:40 +0600

    Add Some New Model

--- HR-Application/DLL/DataContext/ApplicationDbContext.cs
+++ HR-Application/DLL/DataContext/ApplicationDbContext.cs
@@ -12,5 +12,8 @@ namespace DLL.DataContext
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeSalary> EmployeeSalarys { get; set; }
+        public DbSet<Employee_Wise_Present_Absent> Employee_Wise_Present_Absents { get; set; }
+        public DbSet<LeaveApplication> LeaveApplications { get; set; }
     }
 }
