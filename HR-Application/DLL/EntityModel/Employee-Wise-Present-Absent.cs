29000fd1ab41adb17f9868ec8495c7967808e459
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-02 16:17:01 +0600

    Add New Model

--- HR-Application/DLL/EntityModel/Employee-Wise-Present-Absent.cs
+++ HR-Application/DLL/EntityModel/Employee-Wise-Present-Absent.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,16 @@ namespace DLL.EntityModel
 {
     public class Employee_Wise_Present_Absent
     {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public DateTime Date { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public int IsPresent { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public int DepartureTime { get; set; }
     }
 }
