bc7767974cbacf076e2af4716cb55b19f6fa6bfa
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-02 16:28:57 +0600

    Add New Model

--- HR-Application/DLL/EntityModel/EmployeeSalary.cs
+++ HR-Application/DLL/EntityModel/EmployeeSalary.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,22 @@ namespace DLL.EntityModel
 {
     public class EmployeeSalary
     {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Month { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public int Year { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public string PostedAT { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public string PostedBy { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public string IsProvided { get; set; }
     }
 }
