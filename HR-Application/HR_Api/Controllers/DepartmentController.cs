259e92ff9e8d9c46d2a66b3d2ea4ed92ed9bc2bd
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-08 23:42:52 +0600

    Add Some Method

--- HR-Application/HR_Api/Controllers/DepartmentController.cs
+++ HR-Application/HR_Api/Controllers/DepartmentController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BLL.Services;
+using BLL.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace HR_Api.Controllers
 {
 
     public class DepartmentController : MainController
     {
+        private readonly IDepartmentService _departmentService;
+
+        public DepartmentController(IDepartmentService employeeService)
+        {
+            _departmentService = employeeService;
+        }
+
+        
+
+      
+
+      
+
+       
+
+       
     }
 }
