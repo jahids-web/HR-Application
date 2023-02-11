b54164d046d1614901b9f7b5687323bd3336d45b
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-11 23:45:28 +0600

    Remove Migration File

--- HR-Application/DLL/Migrations/20230211173055_ApiMigration.Designer.cs
+++ HR-Application/DLL/Migrations/20230211173055_ApiMigration.Designer.cs
@@ -3,14 +3,16 @@ using DLL.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DLL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230211173055_ApiMigration")]
+    partial class ApiMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
