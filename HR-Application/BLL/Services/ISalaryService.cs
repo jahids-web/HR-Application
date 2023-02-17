9d50525d0e6a4a7c4be60c8c8f9c2b1aa886b93f
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-17 23:55:23 +0600

    Add New Services

---
+++ HR-Application/BLL/Services/ISalaryService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface ISalaryService
+    {
+    
+    }
+}
