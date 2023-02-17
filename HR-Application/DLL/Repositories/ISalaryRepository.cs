c978f418b5e33598b41b9cffed3a51f36f3d17ff
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-17 23:54:23 +0600

    Add New Migration

---
+++ HR-Application/DLL/Repositories/ISalaryRepository.cs
@@ -0,0 +1,23 @@
+﻿using DLL.DataContext;
+using DLL.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories
+{
+    public interface ISalaryRepository : IRepositoryBase<EmployeeSalary>
+    {
+
+    }
+
+    public class SalaryRepository : RepositoryBase<EmployeeSalary>, ISalaryRepository
+    {
+        public SalaryRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
