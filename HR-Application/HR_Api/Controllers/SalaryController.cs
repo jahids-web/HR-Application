f29a2f950a4ff4a3e95089fcfc9f20357319ee11
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-17 23:52:44 +0600

    Add new Controller

---
+++ HR-Application/HR_Api/Controllers/SalaryController.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Services;
+using DLL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HR_Api.Controllers
+{
+    public class SalaryController : MainController
+    {
+        private readonly ISalaryService _salaryRepository;
+
+        public SalaryController(ISalaryService salaryService)
+        {
+            _salaryRepository = salaryService;
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Insert(SelaryViewModel )
+
+    }
+}
