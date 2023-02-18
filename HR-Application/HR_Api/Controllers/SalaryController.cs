b031e0c668aaddbcf85e29d508981c1f5b3a9873
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-18 23:27:48 +0600

    Add New Controller

--- HR-Application/HR_Api/Controllers/SalaryController.cs
+++ HR-Application/HR_Api/Controllers/SalaryController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.Services;
+using BLL.ViewModel;
 using DLL.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,15 +9,41 @@ namespace HR_Api.Controllers
 {
     public class SalaryController : MainController
     {
-        private readonly ISalaryService _salaryRepository;
+        private readonly ISalaryService _salaryService;
 
         public SalaryController(ISalaryService salaryService)
         {
-            _salaryRepository = salaryService;
+            _salaryService = salaryService;
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Insert(SelaryViewModel )
+        [HttpPost]
+        public async Task<IActionResult> Insert(SalaryViewModel request)
+        {
+            return Ok(await _salaryService.InsertAsync(request));
+        }
 
+        [HttpGet("employeSalaryId")]
+        public async Task<IActionResult> GetA(string employeeSalaryId)
+        {
+            return Ok(await _salaryService.GetAAsync(employeeSalaryId));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok (await _salaryService.GetAllAsync());     
+        }
+
+        [HttpPut("employeeSalaryId")]
+        public async Task<IActionResult> Update(string employeeSalaryId, SalaryViewModel requestData)
+        {
+            return Ok(await _salaryService.UpdateAsync(employeeSalaryId, requestData));
+        }
+
+        [HttpDelete("employeSalaryId")]
+        public async Task<IActionResult> Delete(string employeeSalaryId)
+        {
+            return Ok(await _salaryService.DeleteAsync(employeeSalaryId));  
+        }
     }
 }
