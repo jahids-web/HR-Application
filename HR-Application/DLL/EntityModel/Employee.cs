d9918887e366a6ab5a5f6aaf9866129ba8fa9469
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-19 23:48:40 +0600

    Some Changes in Model

--- HR-Application/DLL/EntityModel/Employee.cs
+++ HR-Application/DLL/EntityModel/Employee.cs
@@ -11,47 +11,33 @@ namespace DLL.EntityModel
         public int Id { get; set; }
 
         [Required]
-        [Column(TypeName = "nvarchar(10)")]
+
         public string EmployeeId { get; set; }
 
         //Department-Table
-        [Column(TypeName = "nvarchar(50)")]
         public string DepartmentName { get; set; }
 
-        [Column(TypeName = "nvarchar(20)")]
         public string Name { get; set; }
 
-        [Column(TypeName = "nvarchar(50)")]
         public string Email { get; set; }
 
-        [Column(TypeName = "nvarchar(50)")]
         public string Role { get; set; }
 
-        [Column(TypeName = "nvarchar(50)")]
         public string Designation { get; set; }
 
-        [Column(TypeName = "nvarchar(5)")]
         public string Status { get; set; }
 
-        [Column(TypeName = "nvarchar(20)")]
         public int TotalYearlyAllocatedleave { get; set; }
 
-        [Column(TypeName = "nvarchar(12)")]
         public int MobileNo { get; set; }
 
-        [Column(TypeName = "nvarchar(15)")]
         public string Leave { get; set; }
 
-        [Column(TypeName = "nvarchar(10)")]
         public string IsEmployed { get; set; }
 
-        [Column(TypeName = "nvarchar(20)")]
         public DateTime JoiningDate { get; set; }
 
-        [Column(TypeName = "nvarchar(20)")]
         public DateTime DeparturedDate { get; set; }
-
-        [Column(TypeName = "nvarchar(5)")]
         public int WorkHour { get; set; }
        
         public int DepartmentId { get; set; } //one employee one department one-one
