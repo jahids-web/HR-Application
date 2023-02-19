67de08b59fb8a8527ee82640664953e96cc57d16
Jahid Uddin <jahiduddin922@gmail.com>
2023-02-19 23:49:08 +0600

    Some Changes in Model

--- HR-Application/DLL/EntityModel/LeaveApplication.cs
+++ HR-Application/DLL/EntityModel/LeaveApplication.cs
@@ -15,37 +15,16 @@ namespace DLL.EntityModel
         public int Id { get; set; }
 
         [Required]
-        [Column(TypeName = "nvarchar(10)")]
         public string LeaveApplicationId { get; set; }
-
-        [Column(TypeName = "nvarchar(20)")]
         public string Name { get; set; }
-
-        [Column(TypeName = "nvarchar(50)")]
         public string DepartmentName { get; set; }
-
-        [Column(TypeName = "nvarchar(20)")]
         public string Subject { get; set; }
-
-        [Column(TypeName = "nvarchar(50)")]
         public string Body { get; set; }
-
-        [Column(TypeName = "nvarchar(12)")]
         public string Status { get; set; }
-
-        [Column(TypeName = "nvarchar(20)")]
         public string From { get; set; }
-
-        [Column(TypeName = "nvarchar(20)")]
         public string To { get; set; }
-
-        [Column(TypeName = "nvarchar(12)")]
         public DateTime ApprovalDate { get; set; }
-
-        [Column(TypeName = "nvarchar(12)")]
         public DateTime ApplicationDate { get; set; }
-
-        [Column(TypeName = "nvarchar(12)")]
         public string LastUpdatedAt { get; set; }
         public Department Department { get; set; }
         public string EmployeeId { get; set; }
