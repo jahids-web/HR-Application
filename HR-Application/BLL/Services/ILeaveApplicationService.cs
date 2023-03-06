13ba01996041ba5c9550681ff56d07006edb3ee0
Jahid Uddin <jahiduddin922@gmail.com>
2023-03-07 00:03:11 +0600

    Add Some Code

--- HR-Application/BLL/Services/ILeaveApplicationService.cs
+++ HR-Application/BLL/Services/ILeaveApplicationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.ViewModel;
+using DLL.EntityModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,39 @@ namespace BLL.Services
 {
     public interface ILeaveApplicationService
     {
+        Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity);
+        Task<List<LeaveApplication>> GetAllAsync();
+        Task<LeaveApplication> GetAAsync(string LeaveApplicationId);
+        Task<LeaveApplication> UpdateAsync(string LeaveApplicationId, LeaveApplicationViewModel requestData);
+        Task<LeaveApplication> DeleteAsync(string LeaveApplicationId);
     }
 
     public class LeaveApplicationService : ILeaveApplicationService
     {
-                
-    }   
+        public Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveApplication> GetAAsync(string LeaveApplicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveApplication>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveApplication> UpdateAsync(string LeaveApplicationId, LeaveApplicationViewModel requestData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveApplication> DeleteAsync(string LeaveApplicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
