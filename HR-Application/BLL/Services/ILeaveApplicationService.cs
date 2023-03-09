3c17750d918e78c2c00d07cc40fb3f118b0be983
Jahid Uddin <jahiduddin922@gmail.com>
2023-03-10 01:14:46 +0600

    LeaveApplicationId

--- HR-Application/BLL/Services/ILeaveApplicationService.cs
+++ HR-Application/BLL/Services/ILeaveApplicationService.cs
@@ -1,10 +1,12 @@
 ﻿using BLL.ViewModel;
 using DLL.EntityModel;
+using DLL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utility.Exceptions;
 
 namespace BLL.Services
 {
@@ -12,36 +14,58 @@ namespace BLL.Services
     {
         Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity);
         Task<List<LeaveApplication>> GetAllAsync();
-        Task<LeaveApplication> GetAAsync(string LeaveApplicationId);
-        Task<LeaveApplication> UpdateAsync(string LeaveApplicationId, LeaveApplicationViewModel requestData);
-        Task<LeaveApplication> DeleteAsync(string LeaveApplicationId);
+        Task<LeaveApplication> GetAAsync(string leaveApplicationId);
+        Task<LeaveApplication> UpdateAsync(string leaveApplicationId, LeaveApplicationViewModel requestData);
+        Task<LeaveApplication> DeleteAsync(string leaveApplicationId);
     }
 
     public class LeaveApplicationService : ILeaveApplicationService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LeaveApplicationService(IUnitOfWork unitOfWork) 
+        { 
+            _unitOfWork = unitOfWork;
+        }
         public Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<LeaveApplication> GetAAsync(string LeaveApplicationId)
+        public async Task<LeaveApplication> GetAAsync(string leaveApplicationId)
+        {
+            var leave = await _unitOfWork.LeaveApplicationRepository.FindSingLeAsync(x => x.LeaveApplicationId== leaveApplicationId);
+            if(leave == null)
+            {
+                throw new ApplicationValidationException("LeaveApp Not Found");
+            }  
+            return leave;
+        }
+
+        public async Task<List<LeaveApplication>> GetAllAsync()
+        {
+            return await _unitOfWork.LeaveApplicationRepository.GetList();
+        }
+
+        public Task<LeaveApplication> UpdateAsync(string leaveApplicationId, LeaveApplicationViewModel requestData)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<LeaveApplication>> GetAllAsync()
+        public async Task<LeaveApplication> DeleteAsync(string leaveApplicationId)
         {
-            throw new NotImplementedException();
-        }
+            var leave = await _unitOfWork.LeaveApplicationRepository.FindSingLeAsync(x => x.LeaveApplicationId == leaveApplicationId);
+            if (leave == null)
+            {
+                throw new ApplicationValidationException("LeaveApp lication Not Found");
+            }
 
-        public Task<LeaveApplication> UpdateAsync(string LeaveApplicationId, LeaveApplicationViewModel requestData)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<LeaveApplication> DeleteAsync(string LeaveApplicationId)
-        {
-            throw new NotImplementedException();
+            _unitOfWork.LeaveApplicationRepository.Delete(leave);
+            if (await _unitOfWork.SaveChangesAsync())
+            {
+                return leave;
+            }
+            throw new ApplicationValidationException("Some Problem for delete data");
         }
 
     }
