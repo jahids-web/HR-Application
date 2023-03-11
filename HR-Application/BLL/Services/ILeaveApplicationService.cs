98fa72c3344ea5506dac934eba31ffb92bf49b30
Jahid Uddin <jahiduddin922@gmail.com>
2023-03-11 21:53:17 +0600

    LeaveApplicationServic Complite

--- HR-Application/BLL/Services/ILeaveApplicationService.cs
+++ HR-Application/BLL/Services/ILeaveApplicationService.cs
@@ -15,7 +15,7 @@ namespace BLL.Services
         Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity);
         Task<List<LeaveApplication>> GetAllAsync();
         Task<LeaveApplication> GetAAsync(string leaveApplicationId);
-        Task<LeaveApplication> UpdateAsync(string leaveApplicationId, LeaveApplicationViewModel requestData);
+        Task<LeaveApplication> UpdateAsync(string leaveApplicationId, LeaveApplicationViewModel leaveRequest);
         Task<LeaveApplication> DeleteAsync(string leaveApplicationId);
     }
 
@@ -27,14 +27,46 @@ namespace BLL.Services
         { 
             _unitOfWork = unitOfWork;
         }
-        public Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity)
+        public async Task<LeaveApplication> InsertAsync(LeaveApplicationViewModel viewEntity)
         {
-            throw new NotImplementedException();
+            var department =
+              await _unitOfWork.DepartmentRepository.FindSingLeAsync(x => x.DepartmentName == viewEntity.DepartmentName);
+            var employee =
+              await _unitOfWork.EmployeeRepository.FindSingLeAsync(x => x.EmployeeId == viewEntity.EmployeeId);
+
+            LeaveApplication leaveData = new LeaveApplication();
+            leaveData.LeaveApplicationId = viewEntity.LeaveApplicationId;
+            leaveData.Name = viewEntity.Name;
+            leaveData.EmployeeId = viewEntity.EmployeeId;
+            leaveData.DepartmentName = viewEntity.DepartmentName;
+            leaveData.Subject = viewEntity.Subject;
+            leaveData.Body = viewEntity.Body;
+            leaveData.Status = viewEntity.Status;
+            leaveData.From = viewEntity.From;
+            leaveData.To = viewEntity.To;
+            leaveData.ApplicationDate = viewEntity.ApplicationDate;
+            leaveData.ApprovalDate = viewEntity.ApprovalDate;
+            leaveData.LastUpdatedAt = viewEntity.LastUpdatedAt;
+
+            try
+            {
+                await _unitOfWork.LeaveApplicationRepository.CreateAsync(leaveData);
+
+                if (await _unitOfWork.SaveChangesAsync())
+                {
+                    return leaveData;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationValidationException("Insert Has Some Problem");
+            }
+            return leaveData;
         }
 
         public async Task<LeaveApplication> GetAAsync(string leaveApplicationId)
         {
-            var leave = await _unitOfWork.LeaveApplicationRepository.FindSingLeAsync(x => x.LeaveApplicationId== leaveApplicationId);
+            var leave = await _unitOfWork.LeaveApplicationRepository.FindSingLeAsync(x => x.LeaveApplicationId == leaveApplicationId);
             if(leave == null)
             {
                 throw new ApplicationValidationException("LeaveApp Not Found");
@@ -47,9 +79,33 @@ namespace BLL.Services
             return await _unitOfWork.LeaveApplicationRepository.GetList();
         }
 
-        public Task<LeaveApplication> UpdateAsync(string leaveApplicationId, LeaveApplicationViewModel requestData)
+        public async Task<LeaveApplication> UpdateAsync(string leaveApplicationId, LeaveApplicationViewModel requestData)
         {
-            throw new NotImplementedException();
+            var leaveApplication = await _unitOfWork.LeaveApplicationRepository.FindSingLeAsync(x => x.LeaveApplicationId == leaveApplicationId);
+            if (leaveApplicationId == null)
+            {
+                throw new ApplicationValidationException("Id Not Found");
+            }
+
+            leaveApplication.LeaveApplicationId = requestData.LeaveApplicationId;
+            leaveApplication.Name = requestData.Name;
+            leaveApplication.EmployeeId = requestData.EmployeeId;
+            leaveApplication.DepartmentName = requestData.DepartmentName;
+            leaveApplication.Subject = requestData.Subject;
+            leaveApplication.Body = requestData.Body;
+            leaveApplication.Status = requestData.Status;
+            leaveApplication.From = requestData.From;
+            leaveApplication.To = requestData.To;
+            leaveApplication.ApplicationDate = requestData.ApplicationDate;
+            leaveApplication.ApprovalDate = requestData.ApprovalDate;
+            leaveApplication.LastUpdatedAt = requestData.LastUpdatedAt;
+
+            _unitOfWork.LeaveApplicationRepository.Update(leaveApplication);
+            if (await _unitOfWork.SaveChangesAsync())
+            {
+                return leaveApplication;
+            }
+            throw new ApplicationValidationException("Update Has Some Problem");
         }
 
         public async Task<LeaveApplication> DeleteAsync(string leaveApplicationId)
